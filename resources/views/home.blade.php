ff9263fdd6ee0b98af4e0aaa1a8a8387d009e3fa
crystalhoo <crystal.hoo8@gmail.com>
2020-03-21 06:49:12 +0800

    architecture of user view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,14 @@
 <main id="main">
   @include('sections.about')
 
+  @inclide('sections.hotels')
+
+  @include('sections.gallery')
+
+  @include('sections.faq')
+
+  @include('sections.contact')
+
 
 </main>
 @endsection
