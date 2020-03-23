66c4849582dcf3266a75db5944c8b09202bb907e
crystalhoo <crystal.hoo8@gmail.com>
2020-03-24 04:10:48 +0800

    draft api in plan

--- app/Http/Controllers/Admin/Api/PlanController.php
+++ app/Http/Controllers/Admin/Api/PlanController.php
@@ -19,4 +19,76 @@ class Plan extends Controller
     //need to create 1 plan collection
         return new PlanCollection($plans);
     }
+
+    //need to add plan request here 
+    public function store(Request $request)
+    {
+        try {
+            $plan = new Plan;
+            $plan->fill($request->all());
+
+            $plan->saveOrFail();
+
+            return response()->json([
+                'id' => $plan->id,
+                'created_at' => $plan->created_at,
+            ], 201);
+        }
+        catch(QueryException $ex) {
+            return response()->json([
+                'message' => $ex->getMessage(),
+            ], 500);
+        }
+        catch(\Exception $ex) {
+            return response()->json([
+                'message' => $ex->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            $plan = Plan::with('schedules')->find($id);
+            if(!$plan) throw new ModelNotFoundException;
+
+            return new PlanResource($plan);
+        }
+        catch(ModelNotFoundException $ex) {
+            return response()->json([
+                'message' => $ex->getMessage(),
+            ], 404);
+        }
+    }
+
+    //need to add plan req here
+    public function update(Request $request, $id)
+    {
+        try {
+            $plan = Plan::find($id);
+            if(!$plan) throw new ModelNotFoundException;
+
+            $plan->fill($request->all());
+
+            $plan->saveOrFail();
+
+            return response()->json(null, 204);
+        }
+        catch(ModelNotFoundException $ex) {
+            return response()->json([
+                'message' => $ex->getMessage(),
+            ], 404);
+        }
+        catch(QueryException $ex) {
+            return response()->json([
+                'message' => $ex->getMessage(),
+            ], 500);
+        }
+        catch(\Exception $ex) {
+            return response()->json([
+                'message' => $ex->getMessage(),
+            ], 500);
+        }
+    }
+
 }
