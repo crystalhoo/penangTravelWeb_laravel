a1bc1a8c16706bcf87488c47f0c0895ee85ebfc1
giap <ryonlim@hotmail.com>
2020-04-09 23:22:05 +0800

    replaceStoragepic

--- routes/api.php
+++ routes/api.php
@@ -30,68 +30,10 @@ Route::middleware(['jwt.auth', 'can:manage-users'])->group(function() {
 
 });
 
-//giap stuff
-// Route::post('schedules', function(Request $request) {
-//     $data = $request->all();
-//         return Schedule::create([
-//              'plan_id' => $data['plan_id'],
-//              'day_number' => $data['day_number'],
-//              'start_time' => $data['start_time'],
-//              'title' => $data['title'],
-//              'full_description' => $data['full_description']
-//         ]);
-// });
-//NOT SURE put which file (authorisation)
-//Grace do part
-// Route::get('/posts/delete', 'PostController@delete')->middleware('can:isAdmin')->name('post.delete');
-// Route::get('/posts/update', 'PostController@update')->middleware('can:isAdmin')->name('post.update');
-// Route::get('/posts/create', 'PostController@create')->middleware('can:isAdmin')->name('post.create');
-
-
-//api plan
-// Route::apiResource('plans', 'PlanController');
-// Route::apiResource('hotels', 'HotelController');
-// Route::apiResource('schedules', 'ScheduleController');
-
-// Route::apiResource('plans', 'PlanController');
-// Route::apiResource('hotels', 'HotelController');
-// Route::apiResource('schedules', 'ScheduleController');
 Route::post('galleries/media', 'GalleriesApiController@storeMedia')->name('admin.galleries.storeMedia');
 Route::apiResource('galleries', 'GalleriesApiController');
 
-//hotel actions
-// Route::post('/admin/hotel/create',['as' => 'admin.hotels.create', 'uses' => 'HotelController@create']);
-// Route::post('/admin/hotel/edit',['as' => 'admin.hotels.store', 'uses' => 'HotelController@store']);
-// Route::put('/admin/hotel/{hotel}',['as' => 'admin.hotels.edit', 'uses' => 'HotelController@edit']);
-// Route::put('/admin/hotel/{hotel}/edit',['as' => 'admin.hotels.update', 'uses' => 'HotelController@update']);
-// Route::delete('admin/hotel/delete/{hotel}',['as' => 'admin.hotels.destroy', 'uses' => 'HotelController@destroy']);
-
-
-//plan actions
-// Route::post('/admin/plan/create',['as' => 'admin.plans.create', 'uses' => 'PlanController@create']);
-// Route::post('/admin/plan/edit',['as' => 'admin.plans.store', 'uses' => 'PlanController@store']);
-// Route::put('/admin/plan/{plan}',['as' => 'admin.plans.edit', 'uses' => 'PlanController@edit']);
-// Route::put('/admin/plan/{plan}/edit',['as' => 'admin.plans.update', 'uses' => 'PlanController@update']);
-// Route::delete('admin/plan/delete/{plan}',['as' => 'admin.plans.destroy', 'uses' => 'PlanController@destroy']);
 
 //gallery actions
 Route::post('/admin/galleries/create',['as' => 'admin.galleries.create', 'uses' => 'Admin\Api\GalleriesApiController@create']);
-// Route::post('/admin/galleries/store',['as' => 'admin.galleries.store', 'uses' => 'Admin\Api\GalleriesApiController@store']);
-// //Route::put('/admin/galleries/{galleries}',['as' => 'admin.galleries.edit', 'uses' => 'Admin\Api\GalleriesApiController@edit']);
-// Route::put('/admin/galleries/{galleries}/edit',['as' => 'admin.galleries.update', 'uses' => 'Admin\Api\GalleriesApiController@update']);
 Route::delete('admin/galleries/delete/{galleries}',['as' => 'admin.galleries.destroy', 'uses' => 'Admin\Api\GalleriesApiController@destroy']);
-
-
-// Route::post('galleries','GalleriesApiController@store')->name('galleries');
-// Route::post('/admin/schedule/create',['as' => 'admin.schedules.create', 'uses' => 'ScheduleController@create']);
-// Route::post('/admin/schedule/edit',['as' => 'admin.schedules.store', 'uses' => 'ScheduleController@store']);
-// Route::post('/admin/schedule/{schedule}',['as' => 'admin.schedules.edit', 'uses' => 'ScheduleController@edit']);
-// Route::post('/admin/schedule/{schedule}/edit',['as' => 'admin.schedules.update', 'uses' => 'ScheduleController@update']);
-// Route::delete('admin/schedule/delete/{schedule}',['as' => 'admin.schedules.destroy', 'uses' => 'ScheduleController@destroy']);
-
-// Route::post('/admin/schedule/create',['as' => 'admin.schedules.create', 'uses' => 'ScheduleController@create']);
-// Route::post('/admin/schedule/edit',['as' => 'admin.schedules.store', 'uses' => 'ScheduleController@store']);
-// Route::post('/admin/schedule/{schedule}',['as' => 'admin.schedules.edit', 'uses' => 'ScheduleController@edit']);
-// Route::post('/admin/schedule/{schedule}/edit',['as' => 'admin.schedules.update', 'uses' => 'ScheduleController@update']);
-// Route::delete('admin/schedule/delete/{schedule}',['as' => 'admin.schedules.destroy', 'uses' => 'ScheduleController@destroy']);
-
