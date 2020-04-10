a35e6823b535bbe057393b3d0acd0c360b028451
giap <ryonlim@hotmail.com>
2020-04-10 11:40:51 +0800

    cleanedApi.php

--- routes/api.php
+++ routes/api.php
@@ -18,18 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//authentication
-// Route::middleware('api')->namespace('Auth')->prefix('auth')->group(function() {
-//     Route::post('login', 'AuthController@login');
-//     Route::post('logout', 'AuthController@logout');
-//     Route::post('refresh', 'AuthController@refresh');
-//     Route::post('me', 'AuthController@me');
-// });
-
-// Route::middleware(['jwt.auth', 'can:manage-users'])->group(function() {
-
-// });
-
 Route::post('galleries/media', 'GalleriesApiController@storeMedia')->name('admin.galleries.storeMedia');
 Route::apiResource('galleries', 'GalleriesApiController');
 
